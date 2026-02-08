de8bf2f86329a10cbf6e9144c7f36e4b9b4033b4
Alexis Rosovsky <arosov@github.io>
2026-02-08 01:36:16 +0100

    feat(infrastructure): integrate JSON schema and setup DTO generation

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,26 +1,42 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import net.pwall.json.kotlin.codegen.gradle.JSONSchemaCodegen
+import net.pwall.json.kotlin.codegen.gradle.JSONSchemaCodegenPlugin
+
+buildscript {
+    dependencies {
+        classpath(libs.json.kotlin.gradle)
+        classpath(libs.json.kotlin.schema.codegen)
+    }
+}
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.kotlinSerialization)
 }
+
+apply<JSONSchemaCodegenPlugin>()
 
 kotlin {
     jvm()
     
     sourceSets {
-        commonMain.dependencies {
-            implementation(libs.kwtransport)
-            implementation(libs.compose.runtime)
-            implementation(libs.compose.foundation)
-            implementation(libs.compose.material3)
-            implementation(libs.compose.ui)
-            implementation(libs.compose.components.resources)
-            implementation(libs.compose.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodelCompose)
-            implementation(libs.androidx.lifecycle.runtimeCompose)
+        commonMain {
+            kotlin.srcDir(layout.buildDirectory.dir("generated/sources/json-kotlin"))
+            dependencies {
+                implementation(libs.kwtransport)
+                implementation(libs.kotlinx.serialization.json)
+                implementation(libs.compose.runtime)
+                implementation(libs.compose.foundation)
+                implementation(libs.compose.material3)
+                implementation(libs.compose.ui)
+                implementation(libs.compose.components.resources)
+                implementation(libs.compose.uiToolingPreview)
+                implementation(libs.androidx.lifecycle.viewmodelCompose)
+                implementation(libs.androidx.lifecycle.runtimeCompose)
+            }
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -45,4 +61,12 @@ compose.desktop {
             packageVersion = "1.0.0"
         }
     }
+}
+
+configure<JSONSchemaCodegen> {
+    configFile.set(file("src/commonMain/resources/codegen-config.json"))
+    inputs {
+        inputFile(file("src/commonMain/resources/schema"))
+    }
+    outputDir.set(layout.buildDirectory.dir("generated/sources/json-kotlin").get().asFile)
 }
